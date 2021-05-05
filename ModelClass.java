564a5ff2067d8c1613dab45edc516fa53efaf3f5
Janardhanee01 <71563178+Janardhanee01@users.noreply.github.com>
2021-05-05 08:11:43 +0530

    Create ModelClass.java

---
+++ ModelClass.java
@@ -0,0 +1,34 @@
+package com.example.markattendance;
+
+public class ModelClass {
+
+    private int sno;
+    private String name;
+    private int year;
+    private String department;
+
+
+    public ModelClass(int sno, String name, int year, String department) {
+        this.sno = sno;
+        this.name = name;
+        this.year = year;
+        this.department = department;
+    }
+
+
+        public int getSno() {
+            return sno;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getYear() {
+            return year;
+        }
+
+        public String getDepartment() {
+            return department;
+        }
+}
